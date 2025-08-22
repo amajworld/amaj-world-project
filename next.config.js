5c3b696ccb74fd3a496adb35ac02fe69752bd726
Bd Funny <keyaa7482@gmail.com>
2025-08-22 11:18:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    return config;
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '4mb', // Increase body size limit for server actions
