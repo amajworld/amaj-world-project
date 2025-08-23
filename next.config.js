a4cbeda807b82c77f364b271c6ca989da82b9c30
Bd Funny <keyaa7482@gmail.com>
2025-08-23 11:52:39 +0000

    Unhandled Runtime Error Error: Invalid src prop (https://i.pravatar.cc/1

--- next.config.js
+++ next.config.js
@@ -39,6 +39,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'myquickurl.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
       }
     ],
   },
